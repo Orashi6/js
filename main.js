a30a271f3ec1c527224b7a9fd8802380d0b71159
Orashi6 <bekb0323@gmail.com>
2024-11-13 17:38:32 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// number = 0
+// var number = 20
+// console.log(number);
+// let numb = 100
+// numb = 10
+// console.log(numb);
+// const numbers = 15
+// console.log(numbers);
+
+
+// let savol = "sevgi"
+// let savol1 = 'Sabohat'
+
+// console.log(savol);
+
+let yoshi = prompt('Yoshingiz nechida' , 'yosh')
+let ismi = prompt('Ismingizni Nima')
+let surname = prompt('Familiyangzni kriting')
+let manzil  = prompt('Yashash manzilingizni kiriting')
+let xobbi = prompt('Yoqtirgan xobbingiz')
+let yoqtirgtanKun = prompt ('sizga qaysi hafta kuni yoqadi')
+let fasl = prompt('sizga qaysi fasl yoqadi')
+let hafta = prompt('bugun haftaning qaysi kuni')
+let vaqt = prompt('hozigi vaqtni daqiqalarin bilan yozing')
+let yoqtirganFan = prompt('maktabda yoqtirgan faningizni yozing')
+let yoqtirmaganFan = prompt('maktabda yoqtirmagan faningizni yozing')
+let coinlar = promp('Marsda nechta coiningiz bor')
+let yonalish = prompt ('Marsda qaysi yonalishni tanladingiz')
+let obHavo = prompt ('bugungi ob-havo qandayligini yozing')
+let zaryad = prompt('telefonigiz quvvati necha %?')
+let pcZaryad = prompt('kompyuteringizni quvattini yozib qoldiring')
+let sinf = prompt ('Maktabda sinfingizda nechi kishi oqiydi')
+let oy = prompt ('tugulgan oyingizni yozing')
+let yil = prompt ('tugilgan yilingizni yozing')
+let kun = prompt ('tugilgan kuningizni yozing')
+let baholash = prompt ('savollarni 1dan 10gacha baholang') 
+
+
+alert(ismi)
+alert(surname)
+alert(yoshi , 'yosh')
+alert(manzil)
+alert(xobbi)
+alert(yoqtirganKun)
+alert(fasl)
+alert(hafta)
+alert(vaqt)
+alert(yoqtirganFan)
+alert(yoqtirmaganFan)
+alert(coinlar)
+alert(yonalish)
+alert(obHavo)
+alert(zaryad)
+alert(pcZaryad)
+alert(sinf)
+alert(oy)
+alert(yil)
+alert(kun)
+alert(baholash)
+
+
+// console.log(Savol);
+
+// console.log(savol, '+', savol1, 'sevgi');
